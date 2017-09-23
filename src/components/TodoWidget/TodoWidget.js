098f6b916912dd72103120588b3d1d9dbf2a50c1
Will Hurley <wjhurley@users.noreply.github.com>
2017-09-22 17:45:00 -0700

    initial setup of todo widget

--- src/components/TodoWidget/TodoWidget.js
+++ src/components/TodoWidget/TodoWidget.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { toggleTodoVisibility } from '../../actions/todoActions';
 
 import Widget from '../common/Widget';
+import TodoIcon from './TodoIcon';
+import TodoModal from './TodoModal';
 
-const TodoWidget = () => {
+const TodoWidget = ({todos, onIconClick}) => {
+  const {todoIconIsVisible, todoIsVisible} = todos;
 
-  return <Widget yPosition="bottom"
-                 xPosition="right">
-    Todo
-  </Widget>
+  const renderIcon = () => {
+    return todoIconIsVisible ?
+      <TodoIcon
+        onIconClick={onIconClick}
+      />
+      : null;
+  };
+
+  const renderModal = () => {
+    return todoIsVisible ? <TodoModal /> : null;
+  };
+
+  return (
+    <Widget
+      yPosition="bottom"
+      xPosition="right"
+    >
+      {renderModal()}
+      {renderIcon()}
+    </Widget>
+  );
 };
 
-export default TodoWidget;
+TodoWidget.propTypes = {
+  onIconClick: PropTypes.func.isRequired
+};
+
+function mapStateToProps({todos}) {
+  return {todos};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onIconClick: () => {
+      dispatch(toggleTodoVisibility());
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoWidget);
